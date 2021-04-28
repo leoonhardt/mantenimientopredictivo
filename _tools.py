6ef0a507ac7a07be2f1232f81960c40744dbc69b
leoonhardt <leonardo.d.garrido.a@gmail.com>
2021-04-27 23:23:48 -0400

    Create _tools.py

---
+++ _tools.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+from sklearn.metrics import confusion_matrix
+
+from scipy.stats import kurtosis
+from scipy.stats import skew
