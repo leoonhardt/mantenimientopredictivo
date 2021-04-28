8720024342a8abd89fbdaaa021530af59cefad18
leoonhardt <leonardo.d.garrido.a@gmail.com>
2021-04-27 23:09:39 -0400

    __init__.py

---
+++ __init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+módulo de utilidades para el curso mantenimiento predictivo
+Leonardo Garrido Arroyo
+
+
+basado en el modulo de utilidades del curso ME7260 Aprendizaje Profundo para el
+Diagnóstico y Pronóstico de Fallas. 2020
+"""
+
+from utils._tools import *
